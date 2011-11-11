5798e51141df73abd4eb3de928b3ba5209c5ade2
paws <me@onpaws.com>
2011-11-11 00:59:43 -0500

    handle user signin failure, use flash.now[:error]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
   
   def create
+	  user = User.authenticate(params[:session][:email],
+							   params[:session][:password])
+	  if user.nil?
+		flash.now[:error] = "Invalid logon"
+		@title = "Sign in"
+		render "new"
+	  else
+		# successful signin, log em in!
+	end
   end
   
   def destroy
